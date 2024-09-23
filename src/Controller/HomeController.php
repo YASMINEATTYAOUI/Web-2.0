59239c4fe4c9bfa21c3c1062dc011b30f5776175
YASMINEATTYAOUI <yassmine.attyaoui@fsb.ucar.tn>
2024-09-23 18:31:11 +0100

    Author

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,16 +9,6 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    /*
-    #[Route('/home', name: 'app_home')] //
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/HomeController.php',
-        ]);
-    }
-    */
 
     #[Route('/home', name: 'app_home')] //
     public function index(): Response
